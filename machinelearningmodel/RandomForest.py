1041408df9e1a294a7e384fc751e709ff3eccf5a
wajidali4907 <malikwajid441@gmail.com>
2023-09-09 14:51:31 +0500

    Update machinelearningmodel/RandomForest.py and add output1.csv and texttocsv/csvmodelwithconfigparams.py

--- machinelearningmodel/RandomForest.py
+++ machinelearningmodel/RandomForest.py
@@ -5,7 +5,7 @@ import math
 from sklearn.metrics import mean_squared_error
 
 # Load your combined CSV data directly
-file_path = '/home/wajid/IPC prediction/output.csv'
+file_path = '/home/wajid/IPC prediction/output1.csv'
 combined_data = pd.read_csv(file_path, delimiter=',', encoding='utf-8')
 
 # Define the input parameters (features) you want to use
@@ -16,7 +16,11 @@ input_parameters =  [
     'numBranches',
     'intAluAccesses',
     'numOps',
-    'model'
+    'model',
+    'L1Icache',
+    'L1Dcache',
+    'L2cache',
+    'pipelinewidth'
 ]
 
 # Define the target variable (y)
